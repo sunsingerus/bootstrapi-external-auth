d9a2b6dfcbfc64795932423500439722dc2c87d1
sunsingerus <sunsingerus@gmail.com>
2017-07-04 18:45:27 +0300

    project structure

---
+++ app/database/migrations/20160706231338_create_auth_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class CreateAuthTable
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+        Capsule::schema()->create('users_external', function($table) {
+            $table->increments('id');
+            $table->string('source');
+            $table->string('source_user_id');
+            $table->integer('user_id');
+        });
+
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+        Capsule::schema()->drop('users_external');
+    }
+}
---
+++ app/src/Model/BaseModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BootstrapiAuth\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+}
+
